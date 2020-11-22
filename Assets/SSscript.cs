b87e8fb15e471ffe6bf29a15e380a8313873c119
khris190 <khris10@interia.pl>
2020-11-22 13:48:35 +0100

    Update SSscript.cs
    map selection script, now movable by mouse and snaps into place

--- Assets/SSscript.cs
+++ Assets/SSscript.cs
@@ -5,31 +5,45 @@ using UnityEngine.UI;
 
 public class SSscript : MonoBehaviour
 {
-    public int order;
-	public Button leftButton;
-	public Button rightButton;
-
-
+	public float order;
 	void Update()
 	{
+		if (Input.GetMouseButton(0))
+		{
+
+			order += Input.GetAxis("Mouse X") / 20;
+		}
+		else
+		{
+			float change = order - Mathf.RoundToInt(order);
+			if (change > 0)
+			{
+                if (change > 0.05f)
+                {
+					order -= 0.05f;
+                }
+                else
+                {
+					order = Mathf.RoundToInt(order);
+				}
+			}
+			else if (change < 0)
+			{
+				if (change < 0.05f)
+				{
+					order += 0.05f;
+				}
+				else
+				{
+					order = Mathf.RoundToInt(order);
+				}
+			}
+		}
+		transform.position = Vector3.right * 40 * order / Mathf.Sqrt(Mathf.Abs(order) + 1) + Vector3.forward * (Mathf.Abs(order) + 1);
+		transform.localScale = new Vector3(1f / (Mathf.Abs(order) + 1), 1f / (Mathf.Abs(order) + 1), 1) * 20;
 	}
 	void Start()
 	{
-		//Button btn = leftButton.GetComponent<Button>();
-		leftButton.onClick.AddListener(TaskOnClickLeft);
-		rightButton.onClick.AddListener(TaskOnClickRight);
 	}
 
-	void TaskOnClickLeft()
-	{
-		order--;
-        transform.localPosition = Vector3.right * 80 * order / Mathf.Sqrt(Mathf.Abs(order) + 1) + Vector3.back * (Mathf.Abs(order) + 1);
-		transform.localScale = new Vector3(1f / (Mathf.Abs(order) + 1), 1f / (Mathf.Abs(order) + 1), 1);
-	}
-	void TaskOnClickRight()
-	{
-		order++;
-		transform.localPosition = Vector3.right * 80 * order / Mathf.Sqrt(Mathf.Abs(order) + 1) + Vector3.back *( Mathf.Abs(order) + 1);
-		transform.localScale = new Vector3(1f / (Mathf.Abs(order) + 1), 1f / (Mathf.Abs(order) + 1), 1);
-	}
 }
