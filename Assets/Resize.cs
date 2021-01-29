d2618557acdc294bc487c5c6e53560b3c35d24ba
m.ryngier <michal.ryngier@gmail.com>
2021-01-29 16:17:33 +0100

    feat: prod

---
+++ Assets/Resize.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resize : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(390, 680, false);
+    }
+}
